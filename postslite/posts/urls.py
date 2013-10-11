9348cd480052f3a8d86bd599500788ce40179575
Daniel <daniel@daniel.(none)>
2013-10-11 14:58:14 -0400

    changes post url route

--- postslite/posts/urls.py
+++ postslite/posts/urls.py
@@ -6,5 +6,5 @@ from posts.views import PostDetailView, PostListView
 urlpatterns = patterns('',
     url(r'^$', PostListView.as_view(), name="list_posts"),
     url(r'^post/create/$', 'posts.views.create_post', name="create_post"),
-    url(r'^post/post/(?P<pk>\d+)/$', PostDetailView.as_view(), name="post_detail"),
+    url(r'^post/(?P<pk>\d+)/$', PostDetailView.as_view(), name="post_detail"),
 )
